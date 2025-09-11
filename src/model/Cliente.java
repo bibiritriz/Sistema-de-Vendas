a2d713aa2ccda2019c54f8e642f8d0e5c93080f6
Isaque Barbosa <isaquebatista400@gmail.com>
2025-09-11 10:46:35 -0300

    :sparkles: feat: Added Client Model

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,14 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 /**
  *
- * @author Beatriz Camargo
+ * @author Isaqu
  */
 public class Cliente {
+    private int codCliente;
+    private String nome;
+    private String endereco;
+    private String email;
+    private String telefone;
+
+    public Cliente(
+            int codCliente, 
+            String nome, 
+            String endereco, 
+            String email, 
+            String telefone
+    ){
+        this.codCliente = codCliente;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.email = email;
+        this.telefone = telefone;
+    };
+
+    public Cliente(
+            String nome, 
+            String endereco, 
+            String email, 
+            String telefone
+    ){
+        this.nome = nome;
+        this.endereco = endereco;
+        this.email = email;
+        this.telefone = telefone;
+    };
+    
+    public Cliente(){};
+    
+    public int getCod(){
+        return this.codCliente;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
     
 }
