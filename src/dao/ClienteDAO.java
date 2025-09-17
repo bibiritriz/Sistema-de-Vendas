fdede7b8fbfa8d8b819b507411470ba1a124791d
Isaque Barbosa <isaquebatista400@gmail.com>
2025-09-17 20:07:44 -0300

    :sparkles: feat: added clienteDAO

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -1,14 +1,101 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
-/**
- *
- * @author Beatriz Camargo
- */
+import connection.Conexao;
+import connection.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Cliente;
+
 public class ClienteDAO {
+    private Conexao conexao;;;;
+    private Connection conn;
+    
+    public ClienteDAO(){
+        conexao = new Conexao();
+        conn = this.conexao.getConexao();
+    }
+    
+    public void inserir(Cliente cl){
+        try {
+            String sql = "INSERT INTO clientes (nome, endereco, email, telefone) VALUES (?,?,?,?)";
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            stmt.setString(1, cl.getNome());
+            stmt.setString(2, cl.getEndereco());
+            stmt.setString(3, cl.getEmail());
+            stmt.setString(4, cl.getTelefone());
+            stmt.execute();
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir cliente: " + e.getMessage());
+        }
+    }
+    
+    public Cliente getCliente(int codCliente){
+        try {
+        String sql = "SELECT * FROM clientes WHERE codCliente = ?";     
+            PreparedStatement stmt = this.conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            stmt.setInt(1, codCliente);
+            ResultSet rs = stmt.executeQuery();
+            rs.first();
+            Cliente cl = new Cliente(rs.getInt('codCliente'), rs.getString('nome'), rs.getString('endereco'), rs.getString('email'), rs.getString('telefone'));
+            return cl;
+        } catch (Exception e) {
+            System.out.println("Erro ao buscar cliente: " + ex.getMessage());
+            return null;
+        }
+    }
+    
+    public List<Cliente> getClientes(){
+        List<Cliente> lista = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM clientes";
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                Cliente cl = new Cliente(
+                    rs.getInt("codCliente"),
+                    rs.getString("nome"),
+                    rs.getString("endereco"),
+                    rs.getString("email"),
+                    rs.getString("telefone")
+                );
+                lista.add(cl);
+            }
+        
+        } catch (SQLException ex) {
+            System.out.println("Erro ao buscar todos os clientes: " + ex.getMessage());
+        }
+        return lista;
+    }
+    
+    public void editar(Cliente cl){
+        try {
+            String sql = "UPDATE clientes set nome=?, endereco=?, email=?, telefone=? WHERE codCliente=?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, cl.getNome());
+            stmt.setString(2, cl.getEndereco());
+            stmt.setString(3, cl.getEmail());
+            stmt.setString(4, cl.getTelefone());
+            stmt.setInt(5, cl.getCod());
+            stmt.execute();
+        } catch (SQLException ex) {
+            System.out.println("Erro ao atualizar cliente: " + ex.getMessage());
+        }
+    }
+    
+    public void excluir(int codCliente){
+        try{
+            String sql = "DELETE FROM clientes WHERE codCliente=?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, codCliente);
+            stmt.execute();
+        }catch(SQLException ex){
+            System.out.println("Erro ao excluir cliente: " + ex.getMessage());
+        }
+    }
+    
     
 }
