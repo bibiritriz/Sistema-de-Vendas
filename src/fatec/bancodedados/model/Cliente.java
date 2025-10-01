922b5a26ca43938b687051e6c49015c996d144d8
Isaque Barbosa <isaquebatista400@gmail.com>
2025-10-01 19:40:37 -0300

    :sparkles: feat: Adapt clienteDAO with new cpf atributte

--- src/fatec/bancodedados/model/Cliente.java
+++ src/fatec/bancodedados/model/Cliente.java
@@ -5,7 +5,6 @@ package fatec.bancodedados.model;
  * @author Isaqu
  */
 public class Cliente {
-    private int codCliente;
     private String nome;
     private int codEndereco;
     private String email;
@@ -13,21 +12,19 @@ public class Cliente {
     private String cpf;
 
     public Cliente(
-            int codCliente, 
             String nome, 
             int codEndereco, 
             String email, 
             String telefone,
             String cpf
     ){
-        this.codCliente = codCliente;
         this.nome = nome;
         this.codEndereco = codEndereco;
         this.email = email;
         this.telefone = telefone;
         this.cpf = cpf;
     };
-
+    
     public String getCpf() {
         return cpf;
     }
@@ -36,25 +33,9 @@ public class Cliente {
         this.cpf = cpf;
     }
 
-    public Cliente(
-            String nome, 
-            int codEndereco, 
-            String email, 
-            String telefone,
-            String cpf
-    ){
-        this.nome = nome;
-        this.codEndereco = codEndereco;
-        this.email = email;
-        this.telefone = telefone;
-        this.cpf = cpf;
-    };
     
     public Cliente(){};
     
-    public int getCod(){
-        return this.codCliente;
-    }
     
     public String getNome() {
         return nome;
@@ -76,9 +57,6 @@ public class Cliente {
         return email;
     }
     
-    public void setCod(int codCliente){
-        this.codCliente = codCliente;
-    }
 
     public void setEmail(String email) {
         this.email = email;
