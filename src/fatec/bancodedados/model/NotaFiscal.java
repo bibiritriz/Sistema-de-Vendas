bece3aa7db1e7b16f58820d041413cd469f2cfeb
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-10-01 23:10:15 -0300

    changes

--- src/fatec/bancodedados/model/NotaFiscal.java
+++ src/fatec/bancodedados/model/NotaFiscal.java
@@ -11,43 +11,18 @@ import java.util.List;
 public class NotaFiscal {
 
     private int codNota;
-<<<<<<< Updated upstream
-    private int codCliente;
-=======
-<<<<<<< Updated upstream
-    private String cpfCliente;
->>>>>>> Stashed changes
-    private Date dataVenda;
-=======
     private Cliente cliente;
     private LocalDateTime dataVenda;
->>>>>>> Stashed changes
     private int qtdTotal;
     private double subTotal;
     private boolean status;
     private List<ProdutoNota> itens;
 
-<<<<<<< Updated upstream
-    public NotaFiscal(int codNota, int codCliente, 
-            Date dataVenda, int qtdTotal, double subTotal,
-            boolean status) {
-        this.codNota = codNota;
-        this.codCliente = codCliente;
-=======
-<<<<<<< Updated upstream
-    public NotaFiscal(int codNota, String cpfCliente, 
-            Date dataVenda, int qtdTotal, double subTotal,
-            boolean status) {
-        this.codNota = codNota;
-        this.cpfCliente = cpfCliente;
-=======
     public NotaFiscal(int codNota, Cliente cliente, 
             LocalDateTime dataVenda, int qtdTotal, double subTotal,
             boolean status) {
         this.codNota = codNota;
         this.cliente = cliente;
->>>>>>> Stashed changes
->>>>>>> Stashed changes
         this.dataVenda = dataVenda;
         this.qtdTotal = qtdTotal;
         this.subTotal = subTotal;
@@ -66,19 +41,9 @@ public class NotaFiscal {
     public void addItem(ProdutoNota item) {
         itens.add(item);
     }
-
-<<<<<<< Updated upstream
-    public void setCodCliente(int codCliente) {
-        this.codCliente = codCliente;
-=======
-<<<<<<< Updated upstream
-    public void setCpfCliente(String cpfCliente) {
-        this.cpfCliente = cpfCliente;
-=======
+    
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     }
 
     public void setDataVenda(LocalDateTime dataVenda) {
@@ -101,18 +66,8 @@ public class NotaFiscal {
         return codNota;
     }
 
-<<<<<<< Updated upstream
-    public int getCodCliente() {
-        return codCliente;
-=======
-<<<<<<< Updated upstream
-    public String getCpfCliente() {
-        return cpfCliente;
-=======
     public Cliente getCliente() {
         return cliente;
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     }
 
     public LocalDateTime getDataVenda() {
